2c580580a82f5029a1c6a19ab161406ea1eeb6bb
Amal-nellanhi <amalnellanhi@gmail.com>
2025-12-20 11:17:12 +0530

    Update: Change hero p text and improved visuals

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,10 +11,11 @@ const Hero = () => {
     <h1 className="logo-subtitle">GCEK</h1>
 
     <h2>Build. Collaborate. Innovate.</h2>
+    <br></br>
     <p>
-      To create an ecosystem where students can collaborate across disciplines,
-      gain hands-on experience, and grow into industry-ready professionals
-      through real-world projects and mentorship.
+      Take the leap from learning to doing — join us and shape tomorrow.
+      Together we build, collaborate, and innovate, turning ideas into real-world impact and preparing the next generation of changemakers.
+
     </p>
 
     <button className="btn">
