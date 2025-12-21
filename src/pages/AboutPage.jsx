6de23bd29313d96d83b6f98a1c3de0a7983eaae5
Alan joseph <alanjoseph52006@gmail.com>
2025-12-21 14:57:31 +0530

    adding changes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AboutUs from "../Components/AboutUs/AboutUs";
+import AboutUs from "../Components/AboutUs/Aboutus";
 import "./AboutPage.css";
 const AboutPage = () => {
   return (
