63dcb52babc50cbfdeffe2b83e7f3d601cbe4a4b
Amal-nellanhi <amalnellanhi@gmail.com>
2026-01-07 15:31:59 +0530

    Update: improved leaderboard logic and registration page

--- src/pages/insightx/Leaderboard.jsx
+++ src/pages/insightx/Leaderboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { supabase } from "../../supabaseClient"; 
 import { Trophy, Medal, ArrowLeft, Search, Loader2 } from "lucide-react";
 import { NavLink } from "react-router-dom";
-import "./insightx.css"; 
+import "./Leaderboard.css"; 
 
 const Leaderboard = () => {
   const [teams, setTeams] = useState([]);
